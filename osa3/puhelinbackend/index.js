3fff190fbf3af68414e7d086bca82493667d337b
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-10-10 16:34:57 +0300

    3.11 done

--- osa3/puhelinbackend/index.js
+++ osa3/puhelinbackend/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
 
 app.use(express.static("dist"));
-app.use(cors());
 app.use(express.json());
 morgan.token("body", function (request) {
   return JSON.stringify(request.body);
