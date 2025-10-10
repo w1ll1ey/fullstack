0a3ad61610be080059713f595e4c7cf92fd46d15
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-10-10 16:14:10 +0300

    add production build

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://puhelinluettelo-wy4q.onrender.com/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
