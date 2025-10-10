7ea5ec6d3e0686580d299b91e53d36bd177d4153
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-10-10 10:33:58 +0300

    3.5 done

--- osa3/puhelinbackend/index.js
+++ osa3/puhelinbackend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: "1",
@@ -51,6 +53,13 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const person = request.body;
+  person.id = String(Math.floor(Math.random() * 1000000));
+  persons = persons.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
