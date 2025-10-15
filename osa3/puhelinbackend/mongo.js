0bac868d3cf6cd345fbd30606f6889562e339ec9
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-10-15 13:36:23 +0300

    3.12 done

---
+++ osa3/puhelinbackend/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://vilpe:${password}@cluster0.gwj6ogk.mongodb.net/puhelinluettelo?retryWrites=true&w=majority&appName=Cluster0
+`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+});
+
+if (process.argv.length < 4) {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length > 3) {
+  person.save().then((result) => {
+    console.log(
+      `added person ${person.name} number ${person.number} to phonebook`
+    );
+    mongoose.connection.close();
+  });
+}
