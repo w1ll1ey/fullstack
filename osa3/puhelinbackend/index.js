3f14aa9441deba0a1e93bee672c0ec3aef436915
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-10-15 14:50:33 +0300

    3.13 done

--- osa3/puhelinbackend/index.js
+++ osa3/puhelinbackend/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const Person = require("./models/person");
 
 app.use(express.static("dist"));
 app.use(express.json());
@@ -43,7 +45,9 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -90,7 +94,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
