fef2e8a0cf9fb0e3782f0e61d61d3dc4e463087b
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-10-16 15:41:34 +0300

    3.20 done

--- osa3/puhelinbackend/models/person.js
+++ osa3/puhelinbackend/models/person.js
@@ -22,6 +22,13 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d{5,}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    minlength: 8,
     required: true,
   },
 });
