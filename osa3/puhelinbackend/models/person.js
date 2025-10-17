569c469609d2fab27a20a5845c88dc7a37ffc25c
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-10-17 15:37:34 +0300

    3.22 done

--- osa3/puhelinbackend/models/person.js
+++ osa3/puhelinbackend/models/person.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -24,21 +24,21 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: (v) => {
-        return /^\d{2,3}-\d{5,}$/.test(v);
+        return /^\d{2,3}-\d{5,}$/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
     minlength: 8,
     required: true,
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
--- osa3/puhelinbackend/mongo.js
+++ osa3/puhelinbackend/mongo.js
@@ -1,45 +1,45 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
 const url = `mongodb+srv://vilpe:${password}@cluster0.gwj6ogk.mongodb.net/puhelinluettelo?retryWrites=true&w=majority&appName=Cluster0
-`;
+`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
   name: process.argv[3],
   number: process.argv[4],
-});
+})
 
 if (process.argv.length < 4) {
   Person.find({}).then((result) => {
-    console.log("phonebook:");
+    console.log('phonebook:')
     result.forEach((person) => {
-      console.log(person.name, person.number);
-    });
-    mongoose.connection.close();
-  });
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
 }
 
 if (process.argv.length > 3) {
-  person.save().then((result) => {
+  person.save().then(() => {
     console.log(
       `added person ${person.name} number ${person.number} to phonebook`
-    );
-    mongoose.connection.close();
-  });
+    )
+    mongoose.connection.close()
+  })
 }
