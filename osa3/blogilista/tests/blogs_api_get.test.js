e3f6435ea9ad6faaac53d5bed595fd9d27b454aa
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-10-23 21:11:38 +0300

    4.9 done

---
+++ osa3/blogilista/tests/blogs_api_get.test.js
@@ -0,0 +1,31 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.testBlogs)
+})
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('api returns the right amount of blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.testBlogs.length)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
