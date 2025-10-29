09f9c2d4eedd9860863d5ecd25bbbdb2baf5c8f1
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-10-29 13:45:27 +0200

    4.19 done

--- osa3/blogilista/controllers/blogs.js
+++ osa3/blogilista/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')){
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -15,7 +24,11 @@ blogRouter.post('/', async (request, response) => {
     response.status(400).end()
   }
 
-  const user = await User.findOne({})
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
     return response.status(400).json({ error: 'userId missing or not valid' })
