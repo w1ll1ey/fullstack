0f0e79d9792aa75e454dfbf3bf9b14fa3fef8f3e
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-11-25 09:28:35 +0200

    5.7 done

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -121,7 +121,11 @@ const App = () => {
       </Togglable>
 
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+        <Blog
+          key={blog.id}
+          blog={blog}
+          name={user.name}
+        />
       )}
     </div>
   )
--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog, name }) => {
+  const [extendedId, setExtendedId] = useState(null)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const infoForm = () => {
+    return (
+      <div>
+        {blog.url}
+        <br />
+        likes {blog.likes} <button>like</button>
+        <br />
+        {name}
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={() => setExtendedId(prev => prev === blog.id ? null : blog.id)}>
+        {extendedId === blog.id ? "hide" : "view"}
+      </button>
+      {extendedId && infoForm()}
+    </div>
+  )
+}
 
 export default Blog
