278f8c5894a791362dd7974034ed67e08346bd9b
w1ll1ey <vili.ilari.kaisto@gmail.com>
2026-09-08 15:52:52 +0300

    5.14 done

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -15,4 +16,32 @@ test('renders content', () => {
     const likes = screen.queryByText("67")
     expect(url).toBeNull()
     expect(likes).toBeNull()
+})
+
+test('renders rest of the content when pressing view', async () => {
+    const blog = {
+        id: '123',
+        title: 'Blog about something',
+        url: 'www.something.com',
+        likes: 67,
+        user: {
+            name: 'Someone'
+        }
+    }
+
+    render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const url = screen.getByText(
+        'www.something.com', { exact: false }
+    )
+    const likes = screen.getByText(
+        '67', { exact: false }
+    )
+    const username = screen.getByText(
+        'Someone', { exact: false }
+    )
 })
--- osa5/bloglist-frontend/src/components/Create.jsx
+++ osa5/bloglist-frontend/src/components/Create.jsx
@@ -1,80 +1,80 @@
 import { useState } from 'react'
 
 const CreateForm = ({
-    blogService,
-    blogs,
-    setBlogs,
-    setNotification,
-    setError,
-    createFormRef
+  blogService,
+  blogs,
+  setBlogs,
+  setNotification,
+  setError,
+  createFormRef
 }) => {
-    const [title, setTitle] = useState('')
-    const [author, setAuthor] = useState('')
-    const [url, setUrl] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
-    const handleSubmit = async (event) => {
-        event.preventDefault()
+  const handleSubmit = async (event) => {
+    event.preventDefault()
 
-        try {
-            const newBlog = await blogService.create({ title, author, url })
-            createFormRef.current.toggleVisibility()
-            setBlogs(blogs.concat(newBlog))
-            setTitle('')
-            setAuthor('')
-            setUrl('')
-            setNotification(`A new blog ${newBlog.title} by ${newBlog.author} added`)
-            setError(false)
-            setTimeout(() => {
-                setNotification(null)
-            }, 5000)
-        } catch {
-            setNotification('Could not add the blog')
-            setError(true)
-            setTimeout(() => {
-                setNotification(null)
-            }, 5000)
-        }
+    try {
+      const newBlog = await blogService.create({ title, author, url })
+      createFormRef.current.toggleVisibility()
+      setBlogs(blogs.concat(newBlog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setNotification(`A new blog ${newBlog.title} by ${newBlog.author} added`)
+      setError(false)
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+    } catch {
+      setNotification('Could not add the blog')
+      setError(true)
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
     }
+  }
 
-    return (
+  return (
+    <div>
+      <h2>Create new</h2>
+
+      <form onSubmit={handleSubmit}>
         <div>
-            <h2>Create new</h2>
-
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>
+          <label>
                         title
-                        <input
-                            type="text"
-                            value={title}
-                            onChange={({ target }) => setTitle(target.value)}
-                        />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        author
-                        <input
-                            type="text"
-                            value={author}
-                            onChange={({ target }) => setAuthor(target.value)}
-                        />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        url
-                    </label>
-                    <input
-                        type="text"
-                        value={url}
-                        onChange={({ target }) => setUrl(target.value)}
-                    />
-                </div>
-                <button type="submit">create</button>
-            </form>
+            <input
+              type="text"
+              value={title}
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </label>
         </div>
-    )
+        <div>
+          <label>
+                        author
+            <input
+              type="text"
+              value={author}
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+                        url
+          </label>
+          <input
+            type="text"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
 }
 
 export default CreateForm
