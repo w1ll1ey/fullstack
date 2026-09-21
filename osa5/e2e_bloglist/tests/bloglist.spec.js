8e282a1b3d187c19276e45848e3310e9ac67e8c7
w1ll1ey <vili.ilari.kaisto@gmail.com>
2026-09-21 12:19:57 +0300

    5.17 done

---
+++ osa5/e2e_bloglist/tests/bloglist.spec.js
@@ -0,0 +1,20 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Testi Käyttäjä',
+        username: 'testi',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Log in to application')).toBeVisible()
+  })
+})
